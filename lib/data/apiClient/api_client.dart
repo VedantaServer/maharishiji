84b795caad95517d092100e94070922b63dda312
VedantaServer <vedantaserver01@gmail.com>
2023-10-27 14:55:29 +0530

    api dynamic now..

--- lib/data/apiClient/api_client.dart
+++ lib/data/apiClient/api_client.dart
@@ -6,11 +6,11 @@ import 'package:http/http.dart' as http;
 class ApiClient {
   Future callApiService(String partUrl) async {
     var baseUrl =  GetStorage().read('serverUrl'); //   'https://maharishiji.net/';
-    String username =  "mlodhi630@gmail.com"; // GetStorage().read('LoggedInUser') ;// "mahagroup1008@gmail.com";
-    String password = "1234567"; // GetStorage().read('LoggedInPassword') ;// // "123456";
+    String username =   GetStorage().read('LoggedInEmail') ;// "mahagroup1008@gmail.com";
+    String password =  GetStorage().read('LoggedInPassword') ;// // "123456";
 
-    // device.write('LoggedInUser', _loggedInUser);
-    // device.write('LoggedInPassword', password);
+     print('LoggedInUser' + GetStorage().read('LoggedInUser'));
+     print('LoggedInPassword' +  GetStorage().read('LoggedInPassword'));
     String basicAuth ='Basic ' + base64.encode(utf8.encode('$username:$password'));
     //Expectation is that each module will send send part of request
      //print(baseUrl + partUrl);
