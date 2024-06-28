e26310e4ec139913adac5271442fceabc4f870ca
Shrikant <Shrikantgoel900@gmail.com>
2024-06-28 13:29:34 +0530

    28-06-2024

--- Ionic-Angular/MaharishiJi/src/pages/dashboard/dashboard.ts
+++ Ionic-Angular/MaharishiJi/src/pages/dashboard/dashboard.ts
@@ -8,13 +8,44 @@ import { Storage } from '@ionic/storage';
   selector: 'page-dashboard',
   templateUrl: 'dashboard.html',
 })
+
+
 export class DashboardPage {
 
+  fullName: any;
+  email : any;
+  contactNumber : any;
+  cityName : any;
+  stateName : any;
+  subscriptionStartDate : any;
+  subscriptionEndDate : any;
+  image:any;
+  showstaffdetail:boolean=false;
   constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DashboardPage');
+    this.storage.get('userDetail').then((userDetailValue) => {
+      if (userDetailValue != null) {
+        this.showstaffdetail = true;
+        this.fullName = userDetailValue.data.fullName;
+        this.email = userDetailValue.data.email;
+        this.contactNumber = userDetailValue.data.contactNumber;
+        this.cityName = userDetailValue.data.cityName;
+        this.stateName = userDetailValue.data.stateName;
+        this.image = userDetailValue.data.image== null? "https://ionicframework.com/docs/img/demos/avatar.svg" :userDetailValue.data.image;
+
+        console.log(userDetailValue.data.subscriptionPayment[0].subscriptionStartDate);
+        console.log(userDetailValue.data.subscriptionPayment[0].subscriptionEndDate);
+        this.subscriptionStartDate =userDetailValue.data.subscriptionPayment[0].subscriptionStartDate[2]+"-"+userDetailValue.data.subscriptionPayment[0].subscriptionStartDate[1] +"-"+userDetailValue.data.subscriptionPayment[0].subscriptionStartDate[0];
+        this.subscriptionEndDate =userDetailValue.data.subscriptionPayment[0].subscriptionEndDate[2]+"-"+userDetailValue.data.subscriptionPayment[0].subscriptionEndDate[1] +"-"+userDetailValue.data.subscriptionPayment[0].subscriptionEndDate[0];
+      }
+      else{
+        this.showstaffdetail = false;
+
+      }
+      
+    });
   }
 
 
@@ -22,6 +53,7 @@ export class DashboardPage {
   {
 
     this.storage.clear();
+    this.showstaffdetail = false;
     
   }
 }
