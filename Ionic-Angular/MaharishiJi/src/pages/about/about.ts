f9bc184ae2931a18362877448f58763ffff28789
Shrikant <Shrikantgoel900@gmail.com>
2024-08-17 13:04:07 +0530

    about and Maharishi

--- Ionic-Angular/MaharishiJi/src/pages/about/about.ts
+++ Ionic-Angular/MaharishiJi/src/pages/about/about.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { OpenWebUrlPage } from '../open-web-url/open-web-url';
+ 
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  headerLogo = "https://maharishiji.net/ui-design/templates/news24/images/presets/preset1/logo-footer.png";
+  headerTitle = "Maharishi Ji"
+  
   constructor(public navCtrl: NavController) {
-
+     
+    
   }
 
 }
